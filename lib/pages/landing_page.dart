6a669f2212a32f0d24564c670a3df12d62c0fdfb
rasul1999 <rasulnabiyev59@gmail.com>
2018-11-24 19:40:16 +0400

    Landing page polished

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -3,35 +3,38 @@ import 'package:flutter/material.dart';
 import 'register_page.dart';
 import 'login_page.dart';
 
-
 class LandingPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-       body: Center(
-         child: Column(
-           children: <Widget>[
-             FlatButton(
-               child: Text('Daxil ol'),
-               onPressed: () {
-                 Navigator.push(context, MaterialPageRoute(
-                   builder: (context) => LoginPage(),
-                 ));
-               },
-             ),
-
-             FlatButton(
-               child: Text('Qeydiyyatdan keç'),
-               onPressed: () {
-                 Navigator.push(context, MaterialPageRoute(
-                   builder: (context) => RegisterPage(),
-                 ));
-               },
-             ),
-           ],
-         ),
-       ),
-     );
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('Daxil ol'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ));
+              },
+            ),
+            FlatButton(
+              child: Text('Qeydiyyatdan keç'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterPage(),
+                    ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
